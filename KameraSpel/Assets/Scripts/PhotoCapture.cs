359c08794660f37113354dcc6964e568ec732be0
Matildoz <matilda.jerreling@adelfors.nu>
2026-03-30 00:09:02 +0200

    fotojobb

--- KameraSpel/Assets/Scripts/PhotoCapture.cs
+++ KameraSpel/Assets/Scripts/PhotoCapture.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Rendering;
@@ -7,10 +8,14 @@ using UnityEngine.UI;
 public class PhotoCapture : MonoBehaviour
 {
     [SerializeField] Image photoDisplayArea;
+    Sprite photo;
     Texture2D screenCapture;
+    [SerializeField] List<Sprite> photos;
+    [SerializeField] List<Image> images;
     [SerializeField] int imageRes = 100;
     [SerializeField] int screenToCaptureX;
-    [SerializeField] int screenToCaptureY;  
+    [SerializeField] int screenToCaptureY;
+    int photosTaken;
     void Start()
     {
        
@@ -31,18 +36,31 @@ public class PhotoCapture : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
         Rect areaToRead = new Rect(0,0, Screen.width, Screen.height);
-       
+        photosTaken++;
         screenCapture.ReadPixels(areaToRead, 0, 0, false);
         screenCapture.Apply();
+        TextureToPhoto(screenCapture);
         SavePhoto();
         
     }
     void SavePhoto()
     {
-        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f,0.0f,Screen.width,Screen.height),new Vector2(0.5f,0.5f),imageRes);
-        photoDisplayArea.sprite = photoSprite;
+        
+      
         byte[] byteArray = screenCapture.EncodeToPNG();
         File.WriteAllBytes(Application.dataPath + "Screenshot", byteArray);
     }
- 
+    public Sprite TextureToPhoto(Texture2D photoCapture)
+    {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, Screen.width, Screen.height), new Vector2(0.5f, 0.5f), imageRes);
+        photos.Add(photoSprite);
+        photoDisplayArea.sprite = photoSprite;
+        return photoSprite;
+    }
+    
+    public void AddPhotoToGallery(Sprite photo)
+    {
+        photos.Add(photo);
+        images[0].sprite = photo;
+    }
 }
