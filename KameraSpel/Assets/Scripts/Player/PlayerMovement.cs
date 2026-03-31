6737f1b609500b9990742e8fdd87c9dbab4d7314
Matildoz <matilda.jerreling@adelfors.nu>
2026-03-31 20:07:22 +0200

    Movement-fix

--- KameraSpel/Assets/Scripts/Player/PlayerMovement.cs
+++ KameraSpel/Assets/Scripts/Player/PlayerMovement.cs
@@ -41,9 +41,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.Rotate(0, lookInput.x * mouseSensitivity * Time.deltaTime, 0);
         //Player rotation
-        bodyRot += lookInput.x * mouseSensitivity * Time.deltaTime;
+      //  bodyRot += lookInput.x * mouseSensitivity * Time.deltaTime;
         headRotation += -lookInput.y * mouseSensitivity * Time.deltaTime;
         headRotation = Mathf.Clamp(headRotation, -maxHeadRotation, maxHeadRotation);
        
