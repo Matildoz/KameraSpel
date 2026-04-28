8ca2d689c0f7bbaff7a7535e6676d0aac900701d
Matildoz <matilda.jerreling@adelfors.nu>
2026-04-28 11:31:19 +0200

    work on the deer AI

--- KameraSpel/Assets/Scripts/AI/DeerAI.cs
+++ KameraSpel/Assets/Scripts/AI/DeerAI.cs
@@ -8,11 +8,10 @@ public class DeerAI : MonoBehaviour
     [SerializeField] GameObject player;
     public enum BehaviourStates {fleeing,aware,resting};
     [SerializeField] BehaviourStates currentState;
-
-    [Range(0f, 100f)]
-    [SerializeField] float[] deerStats;
+       
     [Range(0f, 100f)]
     [SerializeField] float Hunger;
+    [SerializeField] float hungerToEat;
     [Range(0f, 100f)]
     [SerializeField] float Thirst;
     [Range(0f, 100f)]
@@ -26,11 +25,11 @@ public class DeerAI : MonoBehaviour
 
     [SerializeField] float spookRange = 10f;
     [SerializeField] float fearAmountToFlee = 20f;
-    [SerializeField] float spookSpeed = 1f;
+    [SerializeField] float spookRaiseSpeed = 1f;
 
     void Start()
     {
-        Invoke("Eat", Random.Range(0,6f));
+       // Invoke("Eat", Random.Range(0,6f));
         Hunger = statMax;
         Energy = statMax;
         Thirst = statMax;
@@ -40,7 +39,22 @@ public class DeerAI : MonoBehaviour
     void Update()
     {
         DrainStats();
-        DistanceToPlayer();
+
+        if(DistanceToPlayer() <= spookRange)
+        {
+            currentState = BehaviourStates.aware;
+            Fear += Time.deltaTime * spookRaiseSpeed;      
+            if (Fear >= fearAmountToFlee)
+            {
+                Flee();
+            }
+        }
+        else
+        {
+            Fear -= Time.deltaTime * spookRaiseSpeed;
+            //Check needs
+        }
+
     }
 
     void Eat()
@@ -48,18 +62,17 @@ public class DeerAI : MonoBehaviour
         animator.SetTrigger("Eat");
     }
 
-    void DistanceToPlayer()
+    float DistanceToPlayer()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position,player.transform.position);
-        if (distanceToPlayer <= spookRange)
-        {
-            currentState = BehaviourStates.aware;
-            Fear += Time.deltaTime * spookSpeed;
-            if(Fear >= fearAmountToFlee)
-            {
-                currentState = BehaviourStates.fleeing;
-            }
-        }
+        float distanceToPlayer = Vector3.Distance(transform.position,player.transform.position);      
+        return distanceToPlayer;
+    }
+    void Flee()
+    {
+        currentState = BehaviourStates.fleeing;
+    }
+    void SetNewPosition()
+    {
 
     }
     void DrainStats()
--- KameraSpel/Assets/Scripts/SetFocus.cs
+++ KameraSpel/Assets/Scripts/SetFocus.cs
@@ -4,9 +4,11 @@ using UnityEngine.Rendering.Universal;
 
 public class SetFocus : MonoBehaviour
 {
+    [SerializeField] Camera physicalCamera;
     [SerializeField] PlayerMovement playerMovement;
     [SerializeField] GameStates gameStates;
     [SerializeField] Volume volume;
+    [SerializeField] LayerMask focusLayer;
     public float focusDistance;
     Ray raycast;
     float hitDistance;
@@ -22,18 +24,22 @@ public class SetFocus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         raycast = new Ray(transform.position,playerMovement.playerHead.transform.forward *focusDistance);
-        if(Physics.Raycast(raycast,out hit, focusDistance))
+         raycast = new Ray(physicalCamera.transform.position,playerMovement.playerHead.transform.forward *focusDistance);
+        if(Physics.Raycast(raycast,out hit, focusDistance,focusLayer))
         {
             hitDistance = Vector3.Distance(transform.position,hit.point);
             hitObject = hit.transform.gameObject;
             AdjustFocus();
+            Debug.DrawLine(physicalCamera.transform.position, hit.point,Color.green);
+        }
+        else
+        {
+            Debug.DrawRay(physicalCamera.transform.position,playerMovement.playerHead.transform.forward,Color.red);
         }
     }
-
+    //Vill man fokusera manuellt eller automatiskt?
     public void AdjustFocus()
     {
-        dof.focusDistance.value = hitDistance;
-        
+        dof.focusDistance.value = hitDistance;        
     }
 }
