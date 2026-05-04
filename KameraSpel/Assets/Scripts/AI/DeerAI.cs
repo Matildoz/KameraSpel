55a27315a88d0782832b5b38a331dca01f6a4935
Matildoz <matilda.jerreling@adelfors.nu>
2026-05-04 15:58:32 +0200

    Deer AI

--- KameraSpel/Assets/Scripts/AI/DeerAI.cs
+++ KameraSpel/Assets/Scripts/AI/DeerAI.cs
@@ -8,7 +8,9 @@ public class DeerAI : MonoBehaviour
     [SerializeField] GameObject player;
     public enum BehaviourStates {fleeing,aware,resting};
     [SerializeField] BehaviourStates currentState;
-       
+
+    [Header("Stats")]
+    [SerializeField] int statMax;
     [Range(0f, 100f)]
     [SerializeField] float Hunger;
     [SerializeField] float hungerToEat;
@@ -16,34 +18,56 @@ public class DeerAI : MonoBehaviour
     [SerializeField] float Thirst;
     [Range(0f, 100f)]
     [SerializeField] float Energy;
-    [SerializeField] int statMax;
-    [Range(0f, 100f)]
-    [SerializeField] float Fear;
+ 
+ 
     [SerializeField] float hungerDrainSpeed = 0.1f;
     [SerializeField] float thirstDrainSpeed = 0.1f;
     [SerializeField] float energyDrainSpeed = 0.1f;
-
+    [Header("FearSettings")]
+    [Range(0f, 100f)]
+    [SerializeField] float Fear;
     [SerializeField] float spookRange = 10f;
     [SerializeField] float fearAmountToFlee = 20f;
     [SerializeField] float spookRaiseSpeed = 1f;
 
     void Start()
     {
-       // Invoke("Eat", Random.Range(0,6f));
+        // Invoke("Eat", Random.Range(0,6f));
+        Fear = 0;
         Hunger = statMax;
         Energy = statMax;
         Thirst = statMax;
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        DrainStats();
+        //DrainStats();
 
-        if(DistanceToPlayer() <= spookRange)
+       switch (currentState)
+        {
+            case BehaviourStates.resting:
+                //Kolla sina stats för att se vad man behöver
+                //Om hungrig
+                //Ät
+                //Om törstig
+                //Gå till vattenkälla, drick
+                //Om energi är låg men hungrig/törstig
+                //Strunta i energi
+                break;
+
+            case BehaviourStates.aware:
+
+                break;
+
+        }
+      
+        if (DistanceToPlayer() <= spookRange)
         {
             currentState = BehaviourStates.aware;
-            Fear += Time.deltaTime * spookRaiseSpeed;      
+            Fear += Time.deltaTime * spookRaiseSpeed;
+            Debug.Log(Fear);
             if (Fear >= fearAmountToFlee)
             {
                 Flee();
@@ -52,6 +76,10 @@ public class DeerAI : MonoBehaviour
         else
         {
             Fear -= Time.deltaTime * spookRaiseSpeed;
+            if (Fear < 0)
+            {
+                Fear = 0;
+            }
             //Check needs
         }
 
@@ -64,7 +92,7 @@ public class DeerAI : MonoBehaviour
 
     float DistanceToPlayer()
     {
-        float distanceToPlayer = Vector3.Distance(transform.position,player.transform.position);      
+        float distanceToPlayer = Vector3.Distance(transform.position,player.transform.position);
         return distanceToPlayer;
     }
     void Flee()
@@ -80,5 +108,9 @@ public class DeerAI : MonoBehaviour
         Hunger -= Time.deltaTime * hungerDrainSpeed;
         Thirst -= Time.deltaTime * thirstDrainSpeed;
         Energy -= Time.deltaTime * energyDrainSpeed;
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, spookRange);
     }
 }
